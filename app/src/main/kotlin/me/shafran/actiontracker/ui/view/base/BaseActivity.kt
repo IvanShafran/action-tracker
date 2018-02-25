be3d1c4876c6447404b8228affeaa06e291d1c59
Ivan Shafran <shafran.fun@gmail.com>
2018-02-25 21:39:18 +0300

    Add base activity and fragment

---
+++ app/src/main/kotlin/me/shafran/actiontracker/ui/view/base/BaseActivity.kt
@@ -0,0 +1,5 @@
+package me.shafran.actiontracker.ui.view.base
+
+import com.arellomobile.mvp.MvpAppCompatActivity
+
+abstract class BaseActivity : MvpAppCompatActivity()
---
+++ app/src/main/kotlin/me/shafran/actiontracker/ui/view/base/BaseFragment.kt
@@ -0,0 +1,12 @@
+package me.shafran.actiontracker.ui.view.base
+
+import com.arellomobile.mvp.MvpAppCompatFragment
+
+abstract class BaseFragment : MvpAppCompatFragment() {
+
+    /**
+     * Handle back press.
+     * Returns True if back press was handled and should not be handled in activity.
+     */
+    open fun onBackPressed(): Boolean = false
+}
