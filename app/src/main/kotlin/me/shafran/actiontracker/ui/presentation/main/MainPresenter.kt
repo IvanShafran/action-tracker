bc29c580d36026b51e4ccca25ca46d2844f9126c
Ivan Shafran <shafran.fun@gmail.com>
2018-02-25 21:40:20 +0300

    Add empty nav drawer screen

---
+++ app/src/main/kotlin/me/shafran/actiontracker/ui/presentation/main/MainPresenter.kt
@@ -0,0 +1,9 @@
+package me.shafran.actiontracker.ui.presentation.main
+
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpPresenter
+
+@InjectViewState
+class MainPresenter : MvpPresenter<MainView>() {
+
+}
---
+++ app/src/main/kotlin/me/shafran/actiontracker/ui/presentation/main/MainView.kt
@@ -0,0 +1,7 @@
+package me.shafran.actiontracker.ui.presentation.main
+
+import com.arellomobile.mvp.MvpView
+
+interface MainView : MvpView {
+
+}
