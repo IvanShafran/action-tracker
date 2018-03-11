39097f9ebf93b388ea9e390079bb3fb5936694a5
Ivan Shafran <shafran.fun@gmail.com>
2018-03-12 01:25:30 +0300

    Delete navigation drawer and unused resources

--- app/src/main/kotlin/me/shafran/actiontracker/ui/view/main/MainActivity.kt
+++ app/src/main/kotlin/me/shafran/actiontracker/ui/view/main/MainActivity.kt
@@ -1,10 +1,6 @@
 package me.shafran.actiontracker.ui.view.main
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.view.MenuItem
 import com.arellomobile.mvp.presenter.InjectPresenter
 import kotlinx.android.synthetic.main.activity_main.*
 import me.shafran.actiontracker.R
@@ -13,7 +9,7 @@ import me.shafran.actiontracker.ui.presentation.main.MainView
 import me.shafran.actiontracker.ui.view.actions.ActionsFragment
 import me.shafran.actiontracker.ui.view.base.BaseActivity
 
-class MainActivity : BaseActivity(), MainView, NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : BaseActivity(), MainView {
 
     @InjectPresenter
     lateinit var mainPresenter: MainPresenter
@@ -23,51 +19,10 @@ class MainActivity : BaseActivity(), MainView, NavigationView.OnNavigationItemSe
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
-
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.main_content, ActionsFragment())
                 .commit()
     }
 
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
-    }
 }
