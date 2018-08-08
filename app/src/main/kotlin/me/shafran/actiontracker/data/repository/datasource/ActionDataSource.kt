6b0d1d2e9f9cb456f4f569bb97cdaafe4798a8bd
Ivan Shafran <shafran.fun@gmail.com>
2018-08-09 00:24:18 +0300

    Delete actionDataSourceException

--- app/src/main/kotlin/me/shafran/actiontracker/data/repository/datasource/ActionDataSource.kt
+++ app/src/main/kotlin/me/shafran/actiontracker/data/repository/datasource/ActionDataSource.kt
@@ -6,38 +6,21 @@ interface ActionDataSource {
 
     /**
      * @return inserted action id
-     * @throws ActionDataSourceException
      */
     fun insertAction(data: InsertActionData): ActionId
 
     /**
      * @return inserted event id
-     * @throws ActionDataSourceException
      */
     fun insertEvent(data: InsertEventData): EventId
 
-    /**
-     * @throws ActionDataSourceException
-     */
     fun updateAction(data: UpdateActionData)
 
-    /**
-     * @throws ActionDataSourceException
-     */
     fun deleteAction(actionId: Long)
 
-    /**
-     * @throws ActionDataSourceException
-     */
     fun deleteEvent(eventId: Long)
 
-    /**
-     * @throws ActionDataSourceException
-     */
     fun getAction(actionId: Long): Action
 
-    /**
-     * @throws ActionDataSourceException
-     */
     fun getAllActions(): List<Action>
 }
--- app/src/main/kotlin/me/shafran/actiontracker/data/repository/datasource/ActionDataSourceException.kt
+++
@@ -1,6 +0,0 @@
-package me.shafran.actiontracker.data.repository.datasource
-
-class ActionDataSourceException(
-        message: String,
-        cause: Throwable
-) : Exception(message, cause)
